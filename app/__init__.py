b37b8e2a5affb6c01ff820bf1b5802eec22abe1c
Matthew (Dane) Limjoco <mdl259@cornell.edu>
2024-04-11 06:20:28 -0400

    Reorganize directory structure

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+# __init__.py
+
+import psycopg2
+import logging
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+
+    # Create the Flask app
+    app = Flask(__name__)
+
+	# Set logger level
+    app.logger.setLevel(logging.INFO) 
+
+    # Database configuration for SQLAlchemy
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://danelimjoco:Uppt1986!@localhost/ehr_database'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize SQLAlchemy within the app
+    db.init_app(app)
+
+    # Register homepage blueprint
+    from .routes.homepage_routes import homepage_bp
+    app.register_blueprint(homepage_bp)
+
+
+    # Register patient blueprint
+    from .routes.patient_routes import patient_bp
+    app.register_blueprint(patient_bp)
+    
+
+    return app
